754cb8c53855851e50c6058e9d04d0334290f6fa
NicholasBobro <lokim3059@gmail.com>
2025-03-04 21:17:25 +0200

    ASd

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -41,25 +41,25 @@ namespace SyberSnake {
 		virtual void OnPaint(System::Object^ obj, PaintEventArgs^ e) override {
 			Graphics^ g = e->Graphics;
 
-			Brush^ snakeBrush = gcnew SolidBrush(Color::Green);
-			g->FillRectangle(snakeBrush, snakePosition.X, snakePosition.Y, blockSize, blockSize);
-			 
-
 			Brush^ fruitBrush = gcnew SolidBrush(Color::Red);
 			g->FillRectangle(fruitBrush, fruitPosition.X, fruitPosition.Y, blockSize, blockSize);
-			
+
+			Brush^ snakeBrush = gcnew SolidBrush(Color::Green);
+			g->FillRectangle(snakeBrush, snakePosition.X, snakePosition.Y, blockSize, blockSize);
 		}
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 		Point snakePosition;
 		Point fruitPosition;
 		const int blockSize = 20;
-			
+		Timer^ timer;
+		int moveX = 0, moveY = 0;
+
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Required method for Designer support - do not modify
@@ -71,21 +71,29 @@ namespace SyberSnake {
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(400, 400);
-			
+
 			this->Text = L"SyberSnake";
 			this->BackColor = Color::Black;
 			this->ResumeLayout(false);
 
 
 
-			snakePosition = Point(this->ClientSize.Width / 2, this->ClientSize.Height / 2 );
+			snakePosition = Point(100, 100);
 			srand(time(NULL));
 			PlaceFruit();
 
-			this->Paint += gcnew PaintEventHandler(this ,  &MyForm::OnPaint) ;
+			timer = gcnew Timer();
+			timer->Interval = 200; //game speed
+			timer->Tick += gcnew EventHandler(this, &MyForm::OnTimerTick);
+			timer->Start();
+
+
+			this->Paint += gcnew PaintEventHandler(this, &MyForm::OnPaint);
+			this->KeyDown += gcnew KeyEventHandler(this, &MyForm::OnKeyDown);
+
 
 		}
 #pragma endregion
@@ -93,9 +101,52 @@ namespace SyberSnake {
 			int maxX = this->ClientSize.Width / blockSize;
 			int maxY = this->ClientSize.Height / blockSize;
 
-			fruitPosition = Point(rand() % maxX * blockSize, rand() % maxY * blockSize);
+			fruitPosition = Point((rand() % maxX) * blockSize, (rand() % maxY) * blockSize);
 		}
 
+		void OnTimerTick(Object^ obj, EventArgs^ e) {
+			snakePosition.X += moveX * blockSize;
+			snakePosition.Y += moveY * blockSize;
+
+			this->Invalidate();
+		}
+
+		void OnKeyDown(Object^ obj, KeyEventArgs^ e) {
+			switch (e->KeyCode) {
+			case Keys::Up:
+				moveX = 0;
+				moveY = -1;
+				break;
+			case Keys::Down:
+				moveX = 0;
+				moveY = 1;
+				break;
+			case Keys::Left:
+				moveX = -1;
+				moveY = 0;
+				break;
+			case Keys::Right:
+				moveX = 1;
+				moveY = 0;
+				break;
+			case Keys::W:
+				moveX = 0;
+				moveY = -1;
+				break;
+			case Keys::S:
+				moveX = 0;
+				moveY = 1;
+				break;
+			case Keys::A:
+				moveX = -1;
+				moveY = 0;
+				break;
+			case Keys::D:
+				moveX = 1;
+				moveY = 0;
+				break;
+			}
+		}
 
 	};
 }
